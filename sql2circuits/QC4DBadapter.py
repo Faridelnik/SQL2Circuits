bd5be38f6ef99eecba4cb6febd21a8d164689722
Faridelnik <farida.s@quantum-brilliance.com>
2024-02-21 15:21:20 +0100

    updated QC4DB adapter

--- sql2circuits/QC4DBadapter.py
+++ sql2circuits/QC4DBadapter.py
@@ -1,24 +1,22 @@
 import pickle
-try:
-    from antlr4 import *
-except ModuleNotFoundError:
-    print("Please install antlr4-python3-runtime to use the parser.")
+from antlr4 import *
 import json
 import os
 from discopy.grammar.pregroup import Ty, Functor
-from discopy.utils import dumps, loads
+# from discopy.utils import dumps, loads
 from circuit_preparation.diagrams.pregroupFunctorMappings import count_boxes, object_mapping, arrow_mapping
 from circuit_preparation.diagrams.cupRemoveFunctorMappings import cup_remove_arrow_mapping, cup_remove_arrow_mapping2
 from circuit_preparation.diagrams.parser.SQLiteLexer import SQLiteLexer
 from circuit_preparation.diagrams.parser.SQLiteParser import SQLiteParser
 from circuit_preparation.diagrams.parser.SQLiteParserListener import SQLiteParserListener
 from lambeq.ansatz import IQPAnsatz, Sim14Ansatz, Sim15Ansatz, StronglyEntanglingAnsatz
-from training.functions.pennylane_functions import predict_circuit, get_valid_states
+from training.functions.pennylane_functions import get_valid_states
 from training.pennylane_circuit import PennylaneCircuit
 from discopy.quantum.pennylane import to_pennylane
 import numpy as np
 from sympy.core.symbol import Symbol
 from sympy import default_sort_key
+import sys
 
 #what should be in the results:
 # - optimised parameters
@@ -26,7 +24,7 @@ from sympy import default_sort_key
 
 # what should be passed:
 # - number of classes
-# - nae of the database
+# - name of the database
 
 cup_removal_functor = Functor(ob = lambda x: x, ar = lambda f: cup_remove_arrow_mapping(f))
 cup_removal_functor2 = Functor(ob = lambda x: x, ar = lambda f: cup_remove_arrow_mapping2(f))
@@ -151,22 +149,14 @@ def find_pkl_files(root_dir):
                 pkl_files.append(os.path.join(dirpath, filename))
     return pkl_files
 
-if __name__ == "__main__":
-
-    # local_vars = locals()
-    # print("locals ", local_vars)
-
-    # if 'num_classes' in locals():
-    #     print("Number of classes:", num_classes)
-    # else:
-    #     print("Number of classes not provided.")
+def predict_execution_time(query):
 
     num_classes = 4
 
-    this_folder = os.path.abspath(os.getcwd())
-    root_directory = this_folder + "/training/results"
-    keywords = ["F1Data", str(num_classes)]
-
+    #script_dir = os.path.dirname(os.path.abspath(__file__))
+    root_directory = "/qc4db/SQL2Circuits/sql2circuits/training/results"
+    keywords = ["F1Data", str(num_classes)+"_classes"]
+    
     matching_directories = find_directories(root_directory, *keywords)
     # if matching_directories:
     #     print("Matching directories:")
@@ -189,9 +179,12 @@ if __name__ == "__main__":
     with open(file_with_optimized_params, 'rb') as f:
         optimized_params = pickle.load(f)
 
-    query = "SELECT b2.constructorresultsid FROM constructorresults AS b2, drivers AS f6, circuits AS a1, status AS n14, results AS k11, sprintresults AS m13 WHERE b2.status = n14.status AND a1.url = f6.url AND m13.fastestlaptime = k11.fastestlaptime;"
+    #query = "SELECT b2.constructorresultsid FROM constructorresults AS b2, drivers AS f6, circuits AS a1, status AS n14, results AS k11, sprintresults AS m13 WHERE b2.status = n14.status AND a1.url = f6.url AND m13.fastestlaptime = k11.fastestlaptime;"
+        
+    #print("query received by python: ", query)
+
     prediction = perform_full_transformation(query, num_classes, optimized_params)
-    print(prediction)
+    #print(prediction)
 
     with open(directory_with_results + '/training_stats.json', 'r') as f:
         data = json.load(f)
@@ -200,12 +193,91 @@ if __name__ == "__main__":
     cluster_centers_str = data.get('cluster centers', '')
     cluster_centers = [float(center.strip('[]')) for center in cluster_centers_str.split()]
 
-    print("Cluster Centers:", cluster_centers)
+    #print("Cluster Centers:", cluster_centers)
+
+    max_index = np.argmax(prediction)
+    estimated_runtime = cluster_centers[max_index]
+
+    #print("Estimated runtime: ", estimated_runtime)
+
+    return estimated_runtime
+
+
+
+if __name__ == "__main__":
+
+    # local_vars = locals()
+    # print("locals ", local_vars)
+
+    # if 'num_classes' in locals():
+    #     print("Number of classes:", num_classes)
+    # else:
+    #     print("Number of classes not provided.")
+
+    num_classes = 4
+
+    #script_dir = os.path.dirname(os.path.abspath(__file__))
+    root_directory = "/qc4db/SQL2Circuits/sql2circuits/training/results"
+    keywords = ["F1Data", str(num_classes)+"_classes"]
+    
+    matching_directories = find_directories(root_directory, *keywords)
+    # if matching_directories:
+    #     print("Matching directories:")
+    #     for directory in matching_directories:
+    #         print(directory)
+    # else:
+    #     print("No directories found matching the criteria.")
+
+    directory_with_results = matching_directories[0]
+    pkl_files = find_pkl_files(directory_with_results)
+
+    if pkl_files:
+        pkl_files.sort()  # Sorting files alphabetically
+        last_pkl_file = pkl_files[-1]  # Selecting the last file
+        #print("Last .pkl file found:", last_pkl_file)
+    else:
+        print("No .pkl files found in the directory.")
+
+    file_with_optimized_params = last_pkl_file
+    with open(file_with_optimized_params, 'rb') as f:
+        optimized_params = pickle.load(f)
+
+    #query = "SELECT b2.constructorresultsid FROM constructorresults AS b2, drivers AS f6, circuits AS a1, status AS n14, results AS k11, sprintresults AS m13 WHERE b2.status = n14.status AND a1.url = f6.url AND m13.fastestlaptime = k11.fastestlaptime;"
+   
+    print ("This is the name of the script: ", sys.argv[0])
+    print ("Number of arguments: ", len(sys.argv))
+    print ("The arguments are: " , str(sys.argv))
+    query = str(sys.argv)
+
+    prediction = perform_full_transformation(query, num_classes, optimized_params)
+    #print(prediction)
+
+    with open(directory_with_results + '/training_stats.json', 'r') as f:
+        data = json.load(f)
+
+    # Extract the cluster centers
+    cluster_centers_str = data.get('cluster centers', '')
+    cluster_centers = [float(center.strip('[]')) for center in cluster_centers_str.split()]
+
+    #print("Cluster Centers:", cluster_centers)
 
     max_index = np.argmax(prediction)
     estimated_runtime = cluster_centers[max_index]
 
     print("Estimated runtime: ", estimated_runtime)
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
     
 
